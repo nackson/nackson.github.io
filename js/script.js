fcbe3e2b40f214f93756402d5352dc3c26fe7dd5
Tomoya Nakakita <nakakita.tomoya@kosaido.co.jp>
2016-10-27 13:26:03 +0900

    年賀-test

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//modal
+$(function(){
+	//画面中央を計算する関数を実行
+	centerringResize();
+
+
+ 
+	//画面の左上かsectionの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+	$(window).resize(centerringResize);
+	function centerringResize(){
+		var w = $(window).width();
+		var h = $(window).height();
+		var cw = $("section").outerWidth();
+		var ch = $("section").outerHeight();
+
+		//取得した値をcssに追加する
+		$("section").css({
+			"left": ((w - cw)/2) + "px",
+			"top": ((h - ch)/2) + "px"
+		});
+	}
+});
